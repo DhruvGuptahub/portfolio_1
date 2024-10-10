2b171cc6f8c326b2fa58a80a0e95ac1e18c9e542
Dhruv gupta <145158632+dhruv8815@users.noreply.github.com>
2024-10-11 00:14:42 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <div className="container nav_bar"
+                data-aos='fade-down'
+                data-aos-duration='1000'
+            >
+                <div className="left nav_item">Portfolio</div>
+                <div className="right">
+                    <a href="#home" className="nav_item">Home</a><a href="#experience" className="nav_item">Experience</a><a href="#skills" className="nav_item">Skills</a><a href="#projects" className="nav_item">Projects</a><a href="#contacts" className="nav_item">Contacts</a>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
